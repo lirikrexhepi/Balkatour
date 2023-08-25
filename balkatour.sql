f9bdc1520a338fce9a1e6746b68dbdb1ab7ed539
Leart-Ramadani <leart.ramadani06@gmail.com>
2023-08-25 15:20:20 +0200

    Updated the sql script

--- balkatour.sql
+++ balkatour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2023 at 03:15 PM
+-- Generation Time: Aug 25, 2023 at 03:19 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
