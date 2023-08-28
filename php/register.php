aac140f36840d14a73c0eb08433621ec73ce66f2
Gjin Prelvukaj <132224503+GjinPrelvukaj@users.noreply.github.com>
2023-08-29 00:09:21 +0200

    404 Page , register.php small fix and htaccess

--- php/register.php
+++ php/register.php
@@ -99,6 +99,9 @@ $state_data = $state_prep->fetchAll(PDO::FETCH_ASSOC);
                 <button class="prev_btn">Prev</button>
                 <button class="next_btn">Next</button>
             </div>
+            <div class="register-links">
+                <a href="../index.php">Already have an account?</a>
+            </div>
         </article>
     </main>
 
